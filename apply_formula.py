8fc5565208886c0363805cc07c9c75c281a212ad
imranshafiq55 <fa21-bse-032@isbstudent.comsats.edu.pk>
2026-02-15 02:58:34 +0500

    Initial commit

---
+++ apply_formula.py
@@ -0,0 +1,11 @@
+from openpyxl import load_workbook
+
+wb = load_workbook('barchart.xlsx')
+sheet = wb['Report']
+
+min_column = wb.active.min_column
+max_column = wb.active.max_column
+min_row = wb.active.min_row
+max_row = wb.active.max_row
+
+#sheet['BB'] = 
